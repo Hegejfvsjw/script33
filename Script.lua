ad16671ff9d3b56e94aaa449199c4f9600d2dbf7
Hegejfvsjw <132260042+Hegejfvsjw@users.noreply.github.com>
2024-01-01 11:19:41 +0300

    Script.lua

--- Script.lua
+++ Script.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Hegejfvsjw/Script22/main/Script.lua", true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Hegejfvsjw/Script22/main/Script.lua"))()
