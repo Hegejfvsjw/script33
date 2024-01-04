f28b4804a5a5451295e4343f00429d88aa271c6e
Hegejfvsjw <132260042+Hegejfvsjw@users.noreply.github.com>
2024-01-04 18:59:02 +0300

    Script.lua

--- Script.lua
+++ Script.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Hegejfvsjw/Script22/main/Script.lua"))()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/Egorikusa/PremiumScripts/main/0711376b7a732c8d43db2779.lua'))()
